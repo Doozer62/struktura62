186f7b5c363521f3639054651264ca4b16153903
Doozer62 <a.a.kopiev@mail.ru>
2022-08-29 13:24:21 +0300

    Production

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
- AOS.init({
+AOS.init({
  	duration: 800,
  	easing: 'slide',
  	once: true
@@ -9,9 +9,11 @@ jQuery(document).ready(function($) {
 	"use strict";
 
 	
-	$(".loader").delay(1000).fadeOut("slow");
-  $("#overlayer").delay(1000).fadeOut("slow");	
+  $(".loader").delay(2000).fadeOut("slow");
+  $("#overlayer").delay(2000).fadeOut("slow");	
   
+
+
 
 	var siteMenuClone = function() {
 
@@ -432,7 +434,24 @@ function prov() {
 	});
   });
 
+        // Popup Куки ! ! ! -----------
 
+  function checkCookies(){
+    let cookieDate = localStorage.getItem('cookieDate');
+    let cookieNotification = document.getElementById('cookie_notification');
+    let cookieBtn = cookieNotification.querySelector('.cookie_accept');
 
+    // Если записи про кукисы нет или она просрочена на 1 год, то показываем информацию про кукисы
+    if( !cookieDate || (+cookieDate + 31536000000) < Date.now() ){
+        cookieNotification.classList.add('show');
+    }
+
+    // При клике на кнопку, в локальное хранилище записывается текущая дата в системе UNIX
+    cookieBtn.addEventListener('click', function(){
+        localStorage.setItem( 'cookieDate', Date.now() );
+        cookieNotification.classList.remove('show');
+    })
+}
+checkCookies();
 
   
